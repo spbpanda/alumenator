1304fd138f75c40552a14d45bb0876e1c4205ab4
Alexander Koval <akoval@bivgroup.ru>
2025-01-22 19:27:49 +0300

    proxy to site

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,8 @@ module.exports = function (app) {
   app.use(
     '/api', // You can change this to any path you want to proxy
     createProxyMiddleware({
-      target: 'http://localhost:5000', // Backend server URL
+      // target: 'http://localhost:5000', // Backend server URL
+      target: 'https://alumenator-api.netlify.app/.netlify/functions/app', // Backend server URL
       changeOrigin: true,
     })
   );
